5c042225061c6b21be0d308e09a76577c4f683e4
Juan Felipe Méndez <Felipe@MacBook-Pro-de-Juan.local>
2014-09-09 19:15:56 -0500

    ejemplo de codigo en c

---
+++ ejemplo.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdio.h>
+#define NPOINTS 10000
+
+int main(){
+  int *lista;
+  int i;
+  lista = malloc(NPOINTS*sizeof(int));
+
+  for(i=0;i<NPOINTS;i++){
+    lista[i]=0;
+
+}
+
+}
+
+
